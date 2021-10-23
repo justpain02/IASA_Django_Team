8e12411fb0d1ac48b62a2bc9d4969d7b1c18dcc9
justpain02 <justpain02kor@gmail.com>
2021-10-23 19:13:48 +0900

    accounts 앱 삭제

--- ch99/mysite/settings.py
+++ ch99/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountConfig',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
