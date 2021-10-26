ecad667de78976c9feb50831852eb440eacc3991
GGODungiejjo <92909292+GGODungiejjo@users.noreply.github.com>
2021-10-26 17:50:43 +0900

    메인게임페이지 기본구성품 만듬

---
+++ ch99/gamepage/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import MainpageView
+urlpatterns = [
+    path('mainpage/', MainpageView.as_view(), name='maingamepage')
+]
--- ch99/gamepage/views.py
+++ ch99/gamepage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from typing import ClassVar
+from django.views.generic.base import TemplateView
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+#--- TemplateView
+class MainpageView(TemplateView):
+    template_name = 'gamepage/maingamepage.html'
