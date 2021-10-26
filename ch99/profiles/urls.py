d6eb8d89ad4c05052317b6704b20ffcf21022d2e
justpain02 <justpain02kor@gmail.com>
2021-10-26 13:27:46 +0900

    profile 편집 메뉴 완성

--- ch99/profiles/urls.py
+++ ch99/profiles/urls.py
@@ -1,12 +1,7 @@
-from django import views
 from django.urls import path
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from django.views.generic.base import View
-from .views import ProfileView, ProfileUpdateView
-
-# 참고 : https://ldgeao99.tistory.com/118, https://ldgeao99.tistory.com/119
+from .views import ProfileView
 urlpatterns = [
     path('<int:pk>/', login_required(ProfileView.as_view()), name='profile'),
-    path('profile_update/', login_required(ProfileUpdateView.as_view()), name='profile_update')
 ]
