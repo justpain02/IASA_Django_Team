80379a548db546c3846b5106a6befd7d27a8d457
justpain02 <justpain02kor@gmail.com>
2021-11-01 16:15:53 +0900

    gitignore 다시 적용

--- ch99/gamepage/admin.py
+++ ch99/gamepage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Game
 # Register your models here.
+
+@admin.register(Game)
+class GamepageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'room_name', 'leader_name', 'people_num', 'answer')
