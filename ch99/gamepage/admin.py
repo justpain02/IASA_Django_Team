3212a2571fbfefe9d284d59a8c3e20dbfb1c63d6
umin0214 <62896719+umin0214@users.noreply.github.com>
2021-11-16 17:32:59 +0900

    모델수정중

--- ch99/gamepage/admin.py
+++ ch99/gamepage/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Game
 # Register your models here.
 
-@admin.register(Game)
-class GamepageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'room_name', 'leader_name', 'people_num', 'answer')
+# @admin.register(Game)
+# class GamepageAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'room_name', 'leader_name', 'people_num', 'answer')
