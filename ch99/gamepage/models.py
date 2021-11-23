fd2da5ca6a7293ee53e5aed6a17e7c537bf0ba11
justpain02 <justpain02kor@gmail.com>
2021-11-23 10:02:39 +0900

    models.py 다시 복구

--- ch99/gamepage/models.py
+++ ch99/gamepage/models.py
@@ -2,13 +2,21 @@ from django.db import models
 from django.urls import reverse
 from django.conf import settings
 # Create your models here.
+
+def default_button_dict():
+    return {
+
+    }
+
 class Game(models.Model):
     room_name = models.CharField('방 이름',max_length=30)
     leader_name = models.CharField('방장',max_length=30)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='유저', blank=True, null=True)
     people_num = models.ManyToManyField(settings.AUTH_USER_MODEL, default=0, blank=True, related_name='참여수')
     answer = models.CharField('정답',max_length=30 , default=0)
-    
+    button_status = models.JSONField(default = default_button_dict)
+    hangman_status = models.IntegerField(default=1)
+
     def get_absolute_url(self):
         return reverse('list')
 
