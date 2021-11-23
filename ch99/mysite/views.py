5cced15820b1610c7684a20a4f827270dc108dad
GGODungiejjo <92909292+GGODungiejjo@users.noreply.github.com>
2021-11-23 16:46:26 +0900

    mysite에 views에서 빠른시작 관련된 game 데이터베이스 호출 코드 삭제

--- ch99/mysite/views.py
+++ ch99/mysite/views.py
@@ -9,8 +9,8 @@ from django.shortcuts import render
 
 #--- TemplateView
 def HomeView(request):
-    gamelist = Game.objects.all().order_by('-pk')
-    return render(request, 'home.html', {'game':gamelist[0]})
+    
+    return render(request, 'home.html')
 """ class HomeView(TemplateView):
     template_name = 'home.html' """
 
